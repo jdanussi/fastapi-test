25d4bed4893c45d09cf81df5fd96f94fc13664b8
Jorge Danussi <jdanussi@gmail.com>
2024-03-07 20:40:50 -0300

    Modify main2.py

--- main2.py
+++ main2.py
@@ -7,11 +7,11 @@ def read_root():
     return {"message": "Hello, you accessed the root"}
 
 @app.get("/route1")
-def create_item():
+def read_route1():
     return {"message": "You accessed /route1"}
 
 @app.get("/route2")
-def update_item(item_id: int):
+def read_route2():
     return {"message": "You accessed /route2"}
 
 if __name__ == "__main__":
